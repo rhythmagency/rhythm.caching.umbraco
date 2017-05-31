42275ae9e6ce1b2d60c9b8f2d325c54c6f468a24
Nicholas-Westby <nicholas.westby@rhythmagency.com>
2017-05-31 14:20:26 -0700

    For PreviewCacheKeys, account for when a request is sent to the back office.

--- src/Rhythm.Caching.Umbraco/CacheHelper.cs
+++ src/Rhythm.Caching.Umbraco/CacheHelper.cs
@@ -17,11 +17,36 @@
         /// in preview mode or not.
         /// </summary>
         /// <remarks>
-        /// This allows for multiple caches to be used (one for live, and one for preview mode).
+        /// This allows for multiple caches to be used (one for live, one for preview mode,
+        /// and one for the back office).
         /// </remarks>
-        public static string[] PreviewCacheKeys => UmbracoContext.Current.InPreviewMode
-            ? new[] { "Preview" }
-            : new[] { "Live" };
+        public static string[] PreviewCacheKeys
+        {
+            get
+            {
+
+                // Variables.
+                var key = default(string);
+                var context = UmbracoContext.Current;
+                var hasContext = context != null;
+
+                // Is there an Umbraco context?
+                if (hasContext)
+                {
+                    key = context.InPreviewMode
+                        ? "Preview"
+                        : "Live";
+                }
+                else
+                {
+                    key = "Back Office";
+                }
+
+                // Return the key (wrapped in an array).
+                return new[] { key };
+
+            }
+        }
 
         #endregion
 
